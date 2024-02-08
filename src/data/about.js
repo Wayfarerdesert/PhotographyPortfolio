2dd899603d8ed0ed5c2682972a8f13cf2c98f184
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2024-02-09 00:25:20 +0100

    responsive style update

--- src/data/about.js
+++ src/data/about.js
@@ -1,6 +1,6 @@
 const about = [{
     name: "Facundo Santana",
-    description: "Facundo Santana nace en Córdoba, Argentina. Su interés por la fotografía surge a temprana edad; En el año 2014 comenzó a estudiar la carrera fotografía en la Universidad Provincial de Córdoba, Escuela de Artes Aplicadas Lino Enea Spilimbergo. En el año 2016, realizo un taller de fotografía documental en el Instituto de Formación Fotográfica. En la actualidad se sigue formando, y simultaneamente trabaja en diferentes proyectos personales. Se desempeña como fotógrafo freelance y colabora para diferentes medios."
+    description: "Facundo Santana nace en Córdoba, Argentina. Su interés por la fotografía surge a temprana edad; En el año 2014 comenzó a estudiar la carrera fotografía en la Universidad Provincial de Córdoba, Escuela de Artes Aplicadas Lino Enea Spilimbergo. En el año 2016, realizo un taller de fotografía documental en el Instituto de Formación Fotográfica en dicha ciudad. En la actualidad se sigue formando, y simultáneamente trabaja en diferentes proyectos personales. Se desempeña como fotógrafo freelance y colabora para diferentes medios."
 }];
 
 const pages = [{
