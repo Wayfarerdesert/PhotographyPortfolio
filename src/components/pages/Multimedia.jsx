7c0ceb7e5f2a56b33dbac32036e97902c61cdf1c
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2025-02-25 14:21:01 +0100

    Refactor: Update video embed component and error handling

--- src/components/pages/Multimedia.jsx
+++ src/components/pages/Multimedia.jsx
@@ -2,42 +2,52 @@ import React from "react";
 import Breadcrumb from "../layout/Breadcrumb";
 import { pages, imgSource } from "../../data/about";
 
-function Multimedia() {
-  const Video_1 = imgSource[4].multimedia[0].link;
-  const Video_2 = imgSource[4].multimedia[1].link;
+const MultimediaEmbed = ({ link, title }) => {
+  const videoId = link.includes("vimeo")
+    ? link.split("/video/")[1].split("?")[0]
+    : link.split("v=")[1];
 
-  // Extracting video ID from the link
-  const videoId = Video_1.split("v=")[1];
-  const videoId_2 = Video_2.split("v=")[1];
+  const videoSrc = link.includes("vimeo")
+    ? `https://player.vimeo.com/video/${videoId}?h=${videoId}`
+    : `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-container">
+      <iframe
+        title={link.includes("vimeo") ? "Vimeo" : "YouTube"}
+        src={videoSrc}
+        width="100%"
+        height="100%"
+        allowfullscreen
+        style={{ backgroundColor: "black" }}
+      ></iframe>
+      {title && <p>{title}</p>}
+    </div>
+  );
+};
+
+const Multimedia = () => {
+  if (!imgSource || !imgSource[4] || !imgSource[4].multimedia) {
+    return (
+      <p className="mainMultimedia">
+        Error: Multimedia data is missing or malformed.
+      </p>
+    );
+  }
+
+  const multimedia = imgSource[4].multimedia || [];
 
   return (
     <div id="pages">
       <Breadcrumb pageTitle={pages[4].title} />
 
       <div className="mainMultimedia">
-        <div>
-          <iframe
-            title="YouTube Video"
-            width="100%"
-            height="100%"
-            src={`https://www.youtube.com/embed/${videoId}`}
-            allowFullScreen
-          ></iframe>
-          <p>{imgSource[4].multimedia[0].title}</p>
-        </div>
-        <div>
-          <iframe
-            title="YouTube Video"
-            width="100%"
-            height="100%"
-            src={`https://www.youtube.com/embed/${videoId_2}`}
-            allowFullScreen
-          ></iframe>
-          <p>{imgSource[4].multimedia[1].title}</p>
-        </div>
+        {multimedia.map((item, index) => (
+          <MultimediaEmbed key={index} link={item.link} title={item.title} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default Multimedia;
